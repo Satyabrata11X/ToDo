c8ca10af5911a86abf77e712af39e0ffda4be63c
Satyabrata Behera <satyabrata11x@gmail.com>
2024-09-30 18:55:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+// Add event listener for 'Enter' key press
+inputBox.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask(); // Call addTask when 'Enter' is pressed
+    }
+});
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+    saveData();  // Save the updated list after task removal
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
